acd4413a1b6aacde41a0c75d373c3c7968efe541
Robert Dickert <robert.dickert@gmail.com>
2016-08-23 16:32:26 -0600

    Works with apollo-client 0.4.x
    At least with a local meteor-integration at meteor-integration commit 62324fb

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
@@ -9,46 +9,48 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
 
-const App = ({ userId, currentUser }) => {
+const App = ({ userId, currentUser, refetch }) => {
   return (
     <div>
       <LoginButtons visible="true" />
       { userId ? (
         <div>
           <pre>{JSON.stringify(currentUser, null, 2)}</pre>
-          <button onClick={() => currentUser.refetch()}>Refetch!</button>
+          <button onClick={() => refetch()}>Refetch!</button>
         </div>
       ) : 'Please log in!' }
     </div>
   )
 }
 
-// This container brings in Apollo GraphQL data
-const AppWithData = connect({
-  mapQueriesToProps({ ownProps }) {
-    if (ownProps.userId) {
-      return {
-        currentUser: {
-          query: gql`
-            query getUserData ($id: String!) {
-              user(id: $id) {
-                emails {
-                  address
-                  verified
-                }
-                username
-                randomString
-              }
-            }
-          `,
-          variables: {
-            id: ownProps.userId,
-          },
-        },
-      };
+GET_USER_DATA = gql`
+  query ($id: String!) {
+    user(id: $id) {
+      emails {
+        address
+        verified
+      }
+      username
+      randomString
     }
+  }
+`;
+
+const withData = graphql(GET_USER_DATA, {
+  props: ({ownProps, data: {error, loading, user, refetch }}) => {
+    if (loading) return { userLoading: true }; 
+    if (error) return { hasErrors: true };
+    return {
+      currentUser: user,
+      refetch,
+    };
   },
-})(App);
+  options: (ownProps) => {
+    return { variables: { id: ownProps.userId } };
+  },
+});
+
+const AppWithData = withData(App);
 
 // This container brings in Tracker-enabled Meteor data
 const AppWithUserId = createContainer(() => {
